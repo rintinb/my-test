b3a5a7d59614ed422a0a024ee288e238b80392a5
rintinb <dsjang@kmu.ac.kr>
2021-11-14 00:07:04 +0900

    init

---
+++ src/main/java/com/example/H2_Thymeleaf_Test/H2ThymeleafTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.H2_Thymeleaf_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ThymeleafTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ThymeleafTestApplication.class, args);
+	}
+
+}
